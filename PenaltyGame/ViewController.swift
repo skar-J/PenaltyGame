9a1046851a0c4ea7e5a3207deae6041fe54dfdaf
kipsong133 <kipsong133@gmail.com>
2020-11-19 12:17:17 +0900

    Initial Commit

---
+++ PenaltyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PenaltyGame
+//
+//  Created by 김우성 on 2020/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
