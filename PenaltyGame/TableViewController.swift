998327dc3a33760b3fbe75d809f2ad75b1497500
leejongyoung <leejongyoung@icloud.com>
2020-11-20 12:21:50 +0900

    Add first page

---
+++ PenaltyGame/TableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewController.swift
+//  PenaltyGame
+//
+//  Created by 이종영 on 2020/11/20.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
