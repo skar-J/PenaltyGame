b2287501f5b1d918de10629dd6452fa34df3ee23
jiyunban <iknowimgood@naver.com>
2020-11-26 11:41:33 +0900

    CountDownController update

---
+++ PenaltyGame/CountDownViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CountViewController.swift
+//  PenaltyGame
+//
+//  Created by 반지윤 on 2020/11/26.
+//
+
+import UIKit
+
+class CountDownViewController: UIViewController {
+
+    @IBOutlet weak var countDownLabel: UILabel!
+    
+    var counter:Int = 5
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateCounter() {
+        if(counter > 0) {
+            countDownLabel.text = String(counter)
+            //print("\(counter)")
+            counter -= 1
+        }
+    }
+}
