007f66ebbfb96aba23a434777a3374549c17365f
jiyunban <iknowimgood@naver.com>
2020-11-30 12:03:18 +0900

    timer update

--- PenaltyGame/CountDownViewController.swift
+++ PenaltyGame/CountDownViewController.swift
@@ -9,16 +9,25 @@ import UIKit
 
 class CountDownViewController: UIViewController {
 
+    @IBOutlet weak var checkButton: UIButton!
+    
+    
     @IBOutlet weak var countDownLabel: UILabel!
     
     var counter:Int = 5
+//    var timer = NSTimer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+        checkButton.isHidden = true
+        
     }
 
+    
+    
     @objc func updateCounter() {
         if(counter > 0) {
             countDownLabel.text = String(counter)
